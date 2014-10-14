ef7d9eaeef0869dd2f19c6dfd340e8cff2931318
Serhij Korocahnskyj <webzepter@gmail.com>
2014-10-14 18:21:04 +0300

    Add testCase2 method to demonstrate 'pseudo-variable' requirement implementation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "menu.h"
+#include "canvas.h"
+#include <iostream>
 
 void testCase1()
 {
@@ -11,10 +13,26 @@ void testCase1()
     delete mainWindow;
 }
 
+void testCase2()
+{
+    int width = 10;
+    int height = 10;
+    ResizableWindow window(0, 0, "window", width, height);
+    Canvas canvas = window.getCanvas();
+    canvas(5, 5) = 'O';
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+            std::cout << *(canvas(i, j));
+        std::cout << std::endl;
+    }
+}
+
 int main()
 {
 //    testCase1();
-    Menu menu;
-    menu.start();
+    testCase2();
+//    Menu menu;
+//    menu.start();
     return 0;
 }
