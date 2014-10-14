cef7b537732e41710f556c2fdf9d34f0dddb6f15
Serhij Korocahnskyj <webzepter@gmail.com>
2014-10-14 17:49:27 +0300

    Add CanvasWrapper class and implement Canvas::operator()  for 'pseudo-variable' requirement

--- WindowsLib/canvas.cpp
+++ WindowsLib/canvas.cpp
@@ -28,3 +28,33 @@ Canvas::size_type Canvas::getWidth() const
 {
     return canvasWidth;
 }
+
+CanvasWrapper Canvas::operator()(Canvas::size_type i, Canvas::size_type j)
+{
+    return CanvasWrapper(this, i, j);
+}
+
+
+CanvasWrapper::CanvasWrapper(Canvas *canvasPtr, Canvas::size_type i, Canvas::size_type j)
+    : canvas(canvasPtr), row(i), column(j)
+{
+}
+
+CanvasWrapper::~CanvasWrapper()
+{
+}
+
+void CanvasWrapper::operator=(Canvas::const_reference value)
+{
+    replaceValue(&value);
+}
+
+void CanvasWrapper::operator=(Canvas::const_pointer value)
+{
+    replaceValue(value);
+}
+
+void CanvasWrapper::replaceValue(Canvas::const_pointer value)
+{
+    canvas[(canvas->getWidth() * row) + column] = *value;
+}
--- WindowsLib/canvas.h
+++ WindowsLib/canvas.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 
+class CanvasWrapper;
+
 class Canvas : public std::vector<char>
 {
 public:
@@ -13,9 +15,27 @@ public:
     ~Canvas();
 
     size_type getWidth() const;
+    CanvasWrapper operator()(size_type i, size_type j);
 
 private:
     size_type canvasWidth;
+};
+
+class CanvasWrapper
+{
+public:
+    CanvasWrapper(Canvas *canvasPtr, Canvas::size_type i, Canvas::size_type j);
+    ~CanvasWrapper();
+
+    void operator=(Canvas::const_reference value);
+    void operator=(Canvas::const_pointer value);
+
+private:
+    Canvas *canvas;
+    Canvas::size_type row;
+    Canvas::size_type column;
+
+    void replaceValue(Canvas::const_pointer value);
 };
 
 #endif // CANVAS_H
